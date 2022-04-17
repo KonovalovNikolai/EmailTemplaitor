3d898a832253db6b4c91da0b4c42fae07e8cf899
Nikolai <konovv.nikk@gmail.com>
2022-04-17 13:20:05 +0300

    добавление mui

--- src/components/EditableList/EditableList.tsx
+++ src/components/EditableList/EditableList.tsx
@@ -19,13 +19,52 @@ type Props = {
     onChange: (list: ListElement[]) => void
 }
 
-class List {
+class FieldList {
     private list: ListElement[]
     private importantElement: UnDeletableListElement = {
         name: "Email",
         isDeletable: false
     }
 
+    public constructor(list: ListElement[]) {
+        this.list = list
+    }
+
+    public GetList(): ListElement[] {
+        return [this.importantElement, ...this.list]
+    }
+
+    public Replace(element: ListElement, newElement: ListElement): FieldList {
+        if (element === this.importantElement) {
+            return null;
+        }
+
+        const index = this.list.indexOf(element)
+        if (~index) {
+            this.list[index] = newElement
+        }
+
+        return new FieldList(this.list)
+    }
+
+    public Delete(element: ListElement): void {
+        
+    }
+
+    public ContainName(name: string): boolean {
+        if (name === this.importantElement.name) {
+            return true;
+        }
+        
+        for (let index = 0; index < this.list.length; index++) {
+            const element = this.list[index];
+            if (element.name === name) {
+                return true
+            }
+        }
+
+        return false
+    }
 }
 
 export const EditableList = ({ list, onChange }: Props) => {
