f66364e37566003b5f8563c71420660512128b71
KonovalovNikolai <konovv.nikk@gmail.com>
2022-05-18 13:40:23 +0300

    Фикс белой полочки на тёмной теме

--- src/components/StyledComponents/components/AppContainer.tsx
+++ src/components/StyledComponents/components/AppContainer.tsx
@@ -4,4 +4,6 @@ export const AppContainer = styled("div", { name: "AppContainer" })(({theme}) =>
     display: 'flex',
     width: "900px",
     height: "500px",
+
+    backgroundColor: theme.palette.background.default,
 }));
